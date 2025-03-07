a3296dc118602c0bfcd464c48ba7cc8c5c5cc1b6
hp <dannenageswar@gmail.com>
2025-03-07 12:11:26 +0530

    07th March -> Learned Shotcuts of Main & sout

---
+++ src/ex_01_Java_Basic/Lab002_Shortcuts.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Shortcuts {
+    // Type just main + enter
+    public static void main(String[] args) {
+   // Type sout + enter
+        System.out.println("Learned Shotcuts");
+
+    }
+}
