a17abaa8235a0287c015e8d41c34f58caf8e56b5
hp <dannenageswar@gmail.com>
2025-03-16 12:10:17 +0530

    16th March -> Math Problem

---
+++ src/Tasks_Daily/_12March_Math.java
@@ -0,0 +1,11 @@
+package Tasks_Daily;
+
+public class _12March_Math {
+
+    public static void main(String[] args) {
+        int X = 10, Y = 10, Z = 10 ;
+        int P=2;
+       double Result= Math.cbrt(Math.pow(X,P)+Math.pow(Y,P)-Math.abs(Z));
+        System.out.println("Result="+Result);
+    }
+}
